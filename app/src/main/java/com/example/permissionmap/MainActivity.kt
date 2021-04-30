52a750641b497db06d02231c818a7dc7cc7f7ee0
alyfadhel <79104875+alyfadhel@users.noreply.github.com>
2021-04-30 19:29:23 +0200

    added latitude added longitude

--- app/src/main/java/com/example/permissionmap/MainActivity.kt
+++ app/src/main/java/com/example/permissionmap/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.permissionmap
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.DialogInterface
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : BaseActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         if (isPermissionGranted()){
             showUserLocation()
@@ -27,12 +35,14 @@ class MainActivity : BaseActivity() {
                 if (isGranted) {
                     // Permission is granted. Continue the action or workflow in your
                     // app.
+                    showUserLocation()
                 } else {
                     // Explain to the user that the feature is unavailable because the
                     // features requires a permission that the user has denied. At the
                     // same time, respect the user's decision. Don't link to system
                     // settings in an effort to convince the user to change their
                     // decision.
+                    Toast.makeText(this,"Access Denied",Toast.LENGTH_LONG).show()
                 }
             }
 
@@ -50,7 +60,15 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun showUserLocation() {
+        fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    if (location!=null){
+                        Log.e("lat",""+location.latitude)
+                        Log.e("long",""+location.longitude)
+                    }
+                }
         Toast.makeText(this,"Show User Location",Toast.LENGTH_LONG).show()
     }
 
