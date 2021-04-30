832f8276e5b7ce102da413f56f47543bcf619624
alyfadhel <79104875+alyfadhel@users.noreply.github.com>
2021-04-30 19:21:37 +0200

    added permission

--- app/src/main/java/com/example/permissionmap/MainActivity.kt
+++ app/src/main/java/com/example/permissionmap/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.permissionmap
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (isPermissionGranted()){
+            showUserLocation()
+        }else{
+            requestedPermissionGranted()
+        }
+    }
+    val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+
+    private fun requestedPermissionGranted() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                showDialoge(message = "App needs to granted permission to find nearest driver",
+                posActionName = "OK",posAction = DialogInterface.OnClickListener { dialog, which ->
+                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    dialog.dismiss()
+                },negActionName = "NO",negAction = DialogInterface.OnClickListener { dialog, which ->
+                    dialog.dismiss()
+                })
+        }else{
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun showUserLocation() {
+        Toast.makeText(this,"Show User Location",Toast.LENGTH_LONG).show()
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 }
